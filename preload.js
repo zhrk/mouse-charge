0180d8dea6003dca11888f6cf2634bb17c3134c0
zhrk <zshchipets@gmail.com>
2025-03-17 19:54:44 +0400

    ci

--- preload.js
+++ preload.js
@@ -4,3 +4,14 @@ contextBridge.exposeInMainWorld("electron", {
   getCharge: () => ipcRenderer.invoke("getCharge"),
   notify: () => ipcRenderer.invoke("notify"),
 });
+
+ipcRenderer.on("update-available", () => {
+  alert("A new update is available. Downloading now...");
+});
+
+ipcRenderer.on("update-downloaded", () => {
+  const userResponse = confirm("Update downloaded. Restart now?");
+  if (userResponse) {
+    ipcRenderer.send("restart-app");
+  }
+});
